01ed02fe81be288298af3655efd632129e0f023e
Claudio Eden <claudio.eden@gmail.com>
2021-02-25 10:08:08 -0300

    Saving files before refreshing line endings

--- CursoFLUTTER/projeto_perguntas/lib/main.dart
+++ CursoFLUTTER/projeto_perguntas/lib/main.dart
@@ -33,12 +33,12 @@ class PerguntaApp extends StatelessWidget {
             RaisedButton(
               child: Text("Resposta 2"),
               onPressed: () {
-                print("Resposta 02 foi selecionada!");
+                print("Resposta 02 foi selecionada!!!!");
               },
             ),
             RaisedButton(
               child: Text("Resposta 3"),
-              onPressed: () => print("Resposta 03!"),
+              onPressed: () => print("Resposta 03!!!"),
             ),
           ],
         ),

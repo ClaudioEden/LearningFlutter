2b47086fbecda59ded4a1436c199c8f7afe7b24e
Claudio Eden <claudio.eden@gmail.com>
2021-02-25 10:18:58 -0300

    Iniciado

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print("Pergunta respondida!!!");
+  }
+
+  @override
+  // implementando método
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é sua cor favorita?',
+      'Qual é seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Perguntas"),
+          backgroundColor: Colors.blue[500],
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text("Resposta 1"),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text("Resposta 2"),
+              onPressed: () {
+                print("Resposta 02!!!!");
+              },
+            ),
+            RaisedButton(
+              child: Text("Resposta 3"),
+              onPressed: () => print("Resposta 03!!!"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
